dde580c80a7cc43944548d9efc74453f4e376b6e
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-09-22 23:40:38 -0500

    Cambios en semillas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-base/app.component';
 import { HomeComponent } from './home/home.component';
 import {TransferHttpCacheModule} from '@nguniversal/common';
 import { routes } from './routes';
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,7 +2,5 @@ import {HomeComponent} from './home/home.component';
 
 
 export const routes = [
-    { path: '', component: HomeComponent, pathMatch: 'full'},
-    { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
-    { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
+    { path: '', component: HomeComponent, pathMatch: 'full'}
   ]
