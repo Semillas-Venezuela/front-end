8baf9f2129b645156ff4b7e3125a3e4d0bf43c76
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-10-22 02:20:42 -0500

    Semilla animada y otras cositas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,34 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {NgxOAuthModule} from 'ngx-oauth-client';
+
+
 
 import { AppComponent } from './app-base/app.component';
 import { HomeComponent } from './home/home.component';
-import {TransferHttpCacheModule} from '@nguniversal/common';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 import { routes } from './routes';
 import { MapComponent } from './map/map.component';
 import { MenuComponent } from './menu/menu.component';
 
 import { MapService } from './services/map.service';
 
+import { TipoUser } from './registro/tipoUser.component/tipoUser.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MapComponent,
-    MenuComponent
+    MenuComponent,
+    TipoUser
 
   ],
   imports: [
     BrowserModule.withServerTransition({appId: 'my-app'}),
     RouterModule.forRoot(routes),
     TransferHttpCacheModule,
-    NgxOAuthModule
+   
   ],
   providers: [MapService],
   bootstrap: [AppComponent]
--- src/app/services/oAuth.ts
+++ src/app/services/oAuth.ts
@@ -1,11 +1,1 @@
-import {NgxOAuthClient, DefaultHeaders, Configuration} from 'ngx-oauth-client';
-@Configuration({
-  host: 'https://api.semillasvenezuela.org/'
-})
-@DefaultHeaders({
-  'Content-Type': 'application/json',
-  'Accept': 'application/json'
-})
-export class MyApiClient extends NgxOAuthClient {
-
-}
+const hola = 0;
