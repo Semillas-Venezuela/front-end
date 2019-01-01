e6816d5c2de7e296eea6a590e23cb3fdd7f83a71
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2019-01-01 00:21:25 -0500

    Changes

--- src/app/semilla/crearSemilla.component/crearSemilla.component.ts
+++ src/app/semilla/crearSemilla.component/crearSemilla.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { SemillasService } from '../../services/semillas.service';
 import { semillaInfo } from '../../models/semillaInfo';
-
+declare const navigator: any;
+declare const MediaRecorder: any;
 
 @Component({
     selector: 'app-crear-semilla',
@@ -12,10 +13,32 @@ import { semillaInfo } from '../../models/semillaInfo';
 export class CrearSemillaComponent implements OnInit{
     
     public semilla : semillaInfo;
-   
+    public isRecording: boolean = false;
+    private chunks: any = [];
+    private mediaRecorder: any;
 
     constructor(public router: Router, public semillasService : SemillasService){
-        
+        const onSuccess = stream => {
+            this.mediaRecorder = new MediaRecorder(stream);
+            this.mediaRecorder.onstop = e => {
+              const audio = new Audio();
+              const blob = new Blob(this.chunks, { 'type': 'audio/ogg; codecs=opus' });
+              this.chunks.length = 0;
+              audio.src = window.URL.createObjectURL(blob);
+              audio.load();
+              audio.play();
+            };
+      
+            this.mediaRecorder.ondataavailable = e => this.chunks.push(e.data);
+        };
+
+        navigator.getUserMedia = (navigator.getUserMedia ||
+          navigator.webkitGetUserMedia ||
+          navigator.mozGetUserMedia ||
+          navigator.msGetUserMedia);
+    
+        navigator.getUserMedia({ audio: true }, onSuccess, e => console.log(e));
+    
     }
 
     ngOnInit(){
@@ -31,4 +54,14 @@ export class CrearSemillaComponent implements OnInit{
             }
         )
     }
+
+    public record() {
+        this.isRecording = true;
+        this.mediaRecorder.start();
+      }
+    
+      public stop() {
+        this.isRecording = false;
+        this.mediaRecorder.stop();
+      }
 }
