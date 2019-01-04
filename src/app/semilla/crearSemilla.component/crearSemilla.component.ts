53da4d4e2868c2dd553b6d715bf4f1d6bedcbbd0
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2019-01-03 21:26:00 -0500

    cambios

--- src/app/semilla/crearSemilla.component/crearSemilla.component.ts
+++ src/app/semilla/crearSemilla.component/crearSemilla.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SemillasService } from '../../services/semillas.service';
 import { semillaInfo } from '../../models/semillaInfo';
@@ -10,53 +10,53 @@ declare const MediaRecorder: any;
     templateUrl: 'crearSemilla.component.html',
     styleUrls: ['crearSemilla.component.css']
 })
-export class CrearSemillaComponent implements OnInit{
-    
-    public semilla : semillaInfo;
+export class CrearSemillaComponent implements OnInit {
+
+    public semilla: semillaInfo;
     public isRecording: boolean = false;
     private chunks: any = [];
     private mediaRecorder: any;
 
-    constructor(public router: Router, public semillasService : SemillasService){
+    constructor(public router: Router, public semillasService: SemillasService) {
         const onSuccess = stream => {
             this.mediaRecorder = new MediaRecorder(stream);
             this.mediaRecorder.onstop = e => {
-              const audio = new Audio();
-              let blob2 = new Blob(this.chunks, {type: 'audio/mp3'});
-              let file:any = blob2;
-              let blob = new Blob(["something"], {type: 'audio/mp3'})
-              file.lastModifiedDate = new Date();
-              file.name = "fileName";
-                this.semillasService.uploadFile("test",blob);
+                console.log(e)
+                const audio = new Audio();
+                let blob = new Blob(this.chunks, { type: 'audio/mp3' });
+                console.warn(blob)
+                this.semillasService.uploadFile("test", blob);
+                this.chunks.length = 0;
+                audio.src = window.URL.createObjectURL(blob);
+                audio.load();
+                audio.play()
 
-              this.chunks.length = 0;
-              audio.src = window.URL.createObjectURL(blob);
-              
-              audio.load();
-              audio.play();
             };
-      
+
             this.mediaRecorder.ondataavailable = e => this.chunks.push(e.data);
         };
 
         navigator.getUserMedia = (navigator.getUserMedia ||
-          navigator.webkitGetUserMedia ||
-          navigator.mozGetUserMedia ||
-          navigator.msGetUserMedia);
-    
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia);
+
         navigator.getUserMedia({ audio: true }, onSuccess, e => console.log(e));
-    
+
     }
 
-    ngOnInit(){
-        this.semilla=new semillaInfo();
+    ngOnInit() {
+        this.semilla = new semillaInfo();
     }
+    guardar(a, b) {
 
-    crearSemilla(){
+
+    }
+    crearSemilla() {
         console.log(1)
-        
+
         this.semillasService.anadirSemilla(this.semilla).then(
-            ()=>{
+            () => {
                 console.log("Semilla creada");
             }
         )
@@ -65,10 +65,10 @@ export class CrearSemillaComponent implements OnInit{
     public record() {
         this.isRecording = true;
         this.mediaRecorder.start();
-      }
-    
-      public stop() {
+    }
+
+    public stop() {
         this.isRecording = false;
         this.mediaRecorder.stop();
-      }
+    }
 }
