0965d60412c34a1e226339fbed56e89df092b319
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2019-06-10 13:35:40 -0500

    Changes

--- src/app/app-base/app.component.ts
+++ src/app/app-base/app.component.ts
@@ -1,14 +1,20 @@
 import {Component,OnInit} from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
+declare var ga: Function;
 @Component({
   selector: 'app-root',
   templateUrl:'./app.component.html',
   styles: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    constructor(){
-
-    }
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+  }
     ngOnInit(){
       var fixed = document.getElementById('fixed');
 
